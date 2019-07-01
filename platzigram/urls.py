5f9d11e6a841df761d391a5d77aae89fef3b5aa0
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-07-01 15:09:27 -0500

    Add post model with image upload

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,5 +1,7 @@
 """Platzigram URLs."""
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -13,3 +15,6 @@ urlpatterns = [
 
     path('posts/', include('posts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/admin.py
+++ posts/admin.py
@@ -2,3 +2,6 @@
 
 from django.contrib import admin
 
+from posts.models import Post
+
+admin.site.register(Post)
