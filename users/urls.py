6773eb67b014b02b2bb6df8e1223241157888b9b
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-07-01 17:47:46 -0500

    Logout

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ from users import views
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 """Users views."""
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import View
 
 
@@ -23,3 +25,11 @@ class LoginView(View):
         else:
             return render(request, 'users/login.html',
                           {'error': 'Invalid username and password'})
+
+
+@login_required(login_url=reverse_lazy('login'),
+                redirect_field_name='redirect_to')
+def logout_view(request):
+    """Logout user."""
+    logout(request)
+    return redirect('login')
