91b9943860725d342580319ef32ebdddd0079fe0
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-07-01 18:08:03 -0500

    User signup

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,6 @@ from users import views
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
     path('logout/', views.logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,9 +2,13 @@
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.db.utils import IntegrityError
 from django.views import View
+
+from users.models import Profile
 
 
 class LoginView(View):
@@ -25,6 +29,41 @@ class LoginView(View):
         else:
             return render(request, 'users/login.html',
                           {'error': 'Invalid username and password'})
+
+
+class SignupView(View):
+    """Signup view."""
+
+    def get(self, request, *args, **kwargs):
+        """Handle GET request. Send signup template."""
+        return render(request, 'users/signup.html')
+
+    def post(self, request, *args, **kwargs):
+        """Handle POST request. Create user."""
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(
+                request, 'users/signup.html',
+                {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html',
+                          {'error': 'Username is already in use'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
 
 
 @login_required(login_url=reverse_lazy('login'),
